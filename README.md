cb3dde7dabe231f5e9b81be042f732587e93abd7
corbfon <corbin.fonville@gmail.com>
2020-08-15 16:48:55 -0500

    backup once a day to s3

---
+++ README.md
@@ -0,0 +1,8 @@
+# Stats Resources
+
+This repo is for internal tooling, such as database backups or helpful development resources. It is not intended for any user-facing features.
+
+## Commands
+
+Backup prod database
+`sls invoke -f backup -d '{"database": "mnrl"}'`
--- lambdas/backup.js
+++ lambdas/backup.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
-const zlib = require('zlib')
-const aws = require('aws-sdk')
+const zip = require('zip-a-folder')
+const AWS = require('aws-sdk')
 const moment = require('moment')
 const { exec } = require('child_process')
+
+const s3 = new AWS.S3({ apiVersion: '2006-03-01' })
 
 const mongodump = ({ username, password, host, database, outPath }) => {
   return new Promise((resolve, reject) => {
@@ -16,29 +18,39 @@ const mongodump = ({ username, password, host, database, outPath }) => {
   })
 }
 
-const handler = async event => {
-  const { dir } = event
-  const { database } = event
-  const { MONGODB_USERNAME_PROD, MONGODB_HOST_PROD, MONGODB_PASSWORD_PROD } = process.env
-  const outPath = `/tmp/${moment().format('MM-DD-YYYY')}`
-  console.log(outPath)
-  try {
-    const stdout = await mongodump({
-      username: MONGODB_USERNAME_PROD,
-      password: MONGODB_PASSWORD_PROD,
-      host: MONGODB_HOST_PROD,
-      database,
-      outPath,
+const zipFolder = outPath => {
+  return new Promise((resolve, reject) => {
+    const zipName = `${outPath}.zip`
+    zip.zipFolder(outPath, zipName, err => {
+      if (err) reject(err)
+      return resolve(zipName)
     })
-    console.log(fs.readdirSync(outPath))
-    return stdout
-  } catch (err) {
-    console.error(err)
-    return {
-      path: process.env.PATH,
-      dir: fs.readdirSync(dir),
-    }
+  })
+}
+
+const handler = async event => {
+  const { database } = event
+  const { MONGODB_USERNAME_PROD, MONGODB_HOST_PROD, MONGODB_PASSWORD_PROD, BACKUPS_BUCKET } = process.env
+  const dateStr = `${moment().format('MM-DD-YYYY')}-${Date.now()}`
+  const outPath = `/tmp/${dateStr}`
+  await mongodump({
+    username: MONGODB_USERNAME_PROD,
+    password: MONGODB_PASSWORD_PROD,
+    host: MONGODB_HOST_PROD,
+    database,
+    outPath,
+  })
+  const zipped = await zipFolder(outPath)
+  const data = fs.readFileSync(zipped)
+  const bucketParams = {
+    Bucket: BACKUPS_BUCKET,
+    Key: `${database}/${dateStr}.zip`,
+    Body: data,
+    ContentType: 'application/zip',
   }
+  await s3.upload(bucketParams).promise()
+  fs.unlinkSync(zipped)
+  return { uploaded: bucketParams.Key }
 }
 
 module.exports = { handler }
