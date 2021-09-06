b6211533279854c48409d6c0f98fe9dd8e13ef5b
Benjamin Nylund <31191383+bnylund@users.noreply.github.com>
2021-09-06 10:49:13 -0500

    Removed download endpoint, removed debug logs

--- lambdas/plugin.js
+++ lambdas/plugin.js
@@ -1,7 +1,6 @@
 const { s3 } = require('../src/aws')
 
 const handler = async (event, context, callback) => {
-  console.log('event', event, 'context', context, 'callback', callback)
   try {
     const obj = await s3
       .headObject({
@@ -11,25 +10,9 @@ const handler = async (event, context, callback) => {
       .promise()
     const response = {
       statusCode: 200,
-      body: JSON.stringify(obj),
+      body: JSON.stringify({ ...obj, Location: 'https://mncs-plugin-bucket.s3.us-east-2.amazonaws.com/MNCS.dll' }),
     }
     callback(null, response)
-  } catch (err) {
-    callback(null, {
-      statusCode: 500,
-      body: "{'error':'Failed to retrieve object.'}",
-    })
-  }
-}
-
-const download = async event => {
-  try {
-    return {
-      statusCode: 302,
-      headers: {
-        Location: 'https://mncs-plugin-bucket.s3.us-east-2.amazonaws.com/MNCS.dll',
-      },
-    }
   } catch (err) {
     callback(null, {
       statusCode: 500,
